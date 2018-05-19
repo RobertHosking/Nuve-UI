ec4c3cd61b5473dc174102107e89987986dc88eb
Robert Hosking <roberthosking96@gmail.com>
2018-05-19 15:22:48 -0400

    start gulp workflow

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Project UI
 
+# Getting Started
+
+`cd Nuve-UI`
+
+`npm install`
+
+
+
 ### Movie Library
 ![movie library](http://i.imgur.com/4SCl8hu.jpg)
 ### Movie Preview
---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./src/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dest'));
+}); 
