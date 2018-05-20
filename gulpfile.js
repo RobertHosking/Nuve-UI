6af4c609256683716bd306747f1888a9979935c7
Robert Hosking <roberthosking96@gmail.com>
2018-05-20 12:45:30 -0400

    project organized

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,38 @@
-gulp.task('autoprefixer', function () {
-    var postcss      = require('gulp-postcss');
-    var sourcemaps   = require('gulp-sourcemaps');
-    var autoprefixer = require('autoprefixer');
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-ruby-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    webserver = require('gulp-webserver');
 
-    return gulp.src('./src/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([ autoprefixer() ]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dest'));
-}); 
+gulp.task('js', function() {
+  return gulp.src('src/js/main.js')
+    .pipe(jshint('./.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('sass', function () {
+    return sass('src/sass/style.scss', {
+      sourcemap: true,
+      style: 'expanded'
+    })
+    .on('error', function (err) {
+        console.error('Error!', err.message);
+    })
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/js/**/*', ['js']);
+  gulp.watch(['src/sass/**/*'], ['sass']);
+});
+
+gulp.task('webserver', function() {
+    gulp.src('src')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
+});
+
+gulp.task('default', ['watch', 'sass','webserver']);
